a740d911031fc9b1e8956810864fe34fcdcba255
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-24 01:51:09 -0500

    can create svg file
    shapes now has a function to render the svg
    index.js can now write svg

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //dependecies
-const fs = require('fs/promises');
+const { readFile, writeFile } = require('fs/promises');
 const inquirer = require('inquirer');
 const shapes = require('./lib/shapes.js');
 
@@ -35,23 +35,26 @@ function init() {
     inquirer.prompt(questions)
     .then((data) => {
         const {text, textColor, shape, shapeColor} = data;
+        let svg = ``;
         switch (shape) {
             case 'circle':
                 const circle = new shapes.Circle(text, textColor, shape, shapeColor);
-                console.log(circle.render());
-                console.log(circle.renderText());
+                svg = circle.renderSVG(circle.renderText(), circle.render());
                 break;
         
             case 'triangle':
                 const triangle = new shapes.Triangle(text, textColor, shape, shapeColor);
-                console.log(triangle.render());
+                svg = triangle.renderSVG(triangle.renderText(), triangle.render());
                 break;
             case 'square':
                 const square = new shapes.Square(text, textColor, shape, shapeColor);
-                console.log(square.render());
+                svg = square.renderSVG(square.renderText(), square.render());
                 break;
         }
-        
+        return writeFile('./examples/logo.svg',svg);
+    })
+    .catch((err) => {
+        console.log(err,'\nUnable to read data');
     });
 }
 
