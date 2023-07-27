a9aff20dccb33266a7b39b8ebf061470ea6d8414
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-27 00:31:24 -0500

    add first test
    test is the set color function renders the correct element
    made inquirer validate the color
    package.json now uses jest for test

--- index.js
+++ index.js
@@ -2,18 +2,34 @@
 const { writeFile } = require('fs/promises');
 const inquirer = require('inquirer');
 const shapes = require('./lib/shapes.js');
-
+var validateColor = require("validate-color").default;
 // prompts
 const questions = [
     {
         type: 'input',
         message: 'Enter text (up to 3 charcters):',
         name: 'text',
+        validate: (input) => {
+            if (input.length > 3) {
+               return 'Text is greater than 3, enter again';
+            }
+            if (input.length < 1 ) {
+                return 'Text was empty, enter again';
+             }
+            return true;
+        }
     },
     {
         type: 'input',
-        message: 'Enter text color (keyword or hex code):',
+        message: 'Enter text color:',
         name: 'textColor',
+        validate: (input) => {
+            if (!validateColor(input)) {
+               return 'Invalid color, enter again';
+            }
+            return true;
+         }
+        
     },
     {
         type: 'list',
@@ -23,44 +39,41 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'Enter shape color (keyword or hex code):',
+        message: 'Enter shape color:',
         name: 'shapeColor',
+        validate: (input) => {
+            if (!validateColor(input)) {
+               return 'Invalid color, enter again';
+            }
+            return true;
+        }
     }
 ]
 
 //main function
 function init() {
     //start up inquirer, then call writeToFile()
-    new Promise((resolve, reject) =>{
-        inquirer.prompt(questions)
-        .then((data) => {
-            const {text, textColor, shape, shapeColor} = data;
-            if(text.length > 3)
-            {
-                error = new Error(`Text is over 3 characters`);
-                reject(error);
-            }
-            var renderShape;
-            switch (shape) {
-                case 'circle':
-                    renderShape = new shapes.Circle(text, textColor, shape, shapeColor);
-                    break;
-                case 'triangle':
-                    renderShape = new shapes.Triangle(text, textColor, shape, shapeColor);
-                    break;
-                case 'square':
-                    renderShape = new shapes.Square(text, textColor, shape, shapeColor);
-                    break;
-            }
-            return renderShape.makeSVG(renderShape.setText(), renderShape.setColor());   
-        })
-        .then((svg) => writeFile(`./examples/logo.svg`, svg)) 
-        .catch((err) => {
-            console.log(err,'\nUnable to read data');
-        });
-
+    inquirer.prompt(questions)
+    .then((data) => {
+        const {text, textColor, shape, shapeColor} = data;
+        var renderShape;
+        switch (shape) {
+            case 'circle':
+                renderShape = new shapes.Circle(text, textColor, shape, shapeColor);
+                break;
+            case 'triangle':
+                renderShape = new shapes.Triangle(text, textColor, shape, shapeColor);
+                break;
+            case 'square':
+                renderShape = new shapes.Square(text, textColor, shape, shapeColor);
+                break;
+        }
+        return renderShape.makeSVG(renderShape.setText(), renderShape.setColor());   
     })
-    
+    .then((svg) => writeFile(`./examples/logo.svg`, svg)) 
+    .catch((err) => {
+        console.log(err,'\nUnable to read data');
+    });
 }
 
 // Function call to initialize app
